aca21bd8f4738ea0ca632993b12659b63c6b0953
luisdb50 <luisdanieldb4@gmail.com>
2021-05-04 18:43:35 -0400

    Reloj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Clock from './prueba/Clock';
 
 function App() {
   return (
     <div className="App">
-      
+      <Clock />
     </div>
   );
 }
