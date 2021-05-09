4e7399d1d0f40ccc0096875d367ac46a6e5a2cb5
luisdb50 <luisdanieldb4@gmail.com>
2021-05-09 17:19:41 -0400

    Tercera practica de react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import ButtonCount from './prueba/ButtonCount';
+import Container from './prueba/Container';
+
+let texto= "Deseo una pc gamer con case msi vampiric, fuente de poder certificada azza 650w, targeta madre h370m, " + 
+           "procesador intel i5 9400, memoria DDR4 8gb, disco SSD 240gb y targeta de video GTX 1650 4gb"
 
 function App() {
   return (
     <div className="App">
-      <ButtonCount />
+      <Container text={texto} />
     </div>
   );
 }
---
+++ src/prueba/Container.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../App.css';
+
+export default function Container(props){
+    
+
+
+    
+    return(
+        <div className="div_viewport">
+            <div className="div_container">
+                <div className="div_image">
+                    <img src="https://i.pinimg.com/originals/6c/2e/07/6c2e07660c6fcf90d6a362ced622e1ad.png" className="img"></img>
+                </div>
+                <div className="div_text">
+                    <p className="p-titulo">Has programado tanto que te has topado con el chems de la suerte</p>
+                    <br/>
+                    <p className="p-titulo">Pidele un deseo: </p>
+                    <p className="p-deseo">{props.text}</p>
+                    <br/>
+                    <br/>
+                    <p className="p-deseo">Chems: No puede ser...</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
